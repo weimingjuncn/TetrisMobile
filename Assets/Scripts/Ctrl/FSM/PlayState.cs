e3b86fbbc0664c6c1bce55fd4d206c4bccc49007
mingjun <mingjun4503@163.com>
2020-10-04 15:12:15 +0800

    add fsm scripts

---
+++ Assets/Scripts/Ctrl/FSM/PlayState.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayState : FSMState
+{
+    private void Awake()
+    {
+        stateID = StateID.Play;
+        AddTransition(Transition.PauseButtonClick, StateID.Menu);
+    }
+    public override void DoBeforeEntering()
+    {
+        ctrl.view.ShowGameUI(ctrl.model.Score, ctrl.model.HighestScore);
+        ctrl.view.ShowCtrlShapeUI();
+        ctrl.gameManager.StartGame();
+    }
+    public override void DoBeforeLeaving()
+    {
+        ctrl.view.HideGameUI();
+        ctrl.view.HideCtrlShapeUI();
+        ctrl.view.ShowRestartButton();
+
+        ctrl.gameManager.PauseGame();
+    }
+    public void OnPauseButtonClick()
+    {
+        ctrl.audioManager.PlayCursor();
+        fsm.PerformTransition(Transition.PauseButtonClick);
+    }
+
+    public void OnRestartButtonClick()
+    {
+        ctrl.view.HideGameOverUI();
+        ctrl.model.Restart();
+        ctrl.gameManager.StartGame();
+        ctrl.view.UpdateGameUI(0, ctrl.model.HighestScore);
+    }
+}
